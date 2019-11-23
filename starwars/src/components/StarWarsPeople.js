d53b5f4d4c3df1dd51d5e9975e6d98cf4fe8b8b1
David Moates <david.m.moates@gmail.com>
2019-11-22 21:02:01 -0600

    started Styling

--- starwars/src/components/StarWarsPeople.js
+++ starwars/src/components/StarWarsPeople.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import StarWarsCard from './StarWarsCard';
 
 const StarWarsPeople = (props) => {
 
 
+const FlexBox = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 10px;
+`
+
 return (
-  props.people.map((char, index) => <StarWarsCard key = {index} person = {char} />)
+  <FlexBox>
+    {props.people.map((char, index) => <StarWarsCard key = {index} person = {char} />)}
+  </FlexBox>
 )
 
 }
