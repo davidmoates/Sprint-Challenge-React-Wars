c5bc3e7d7cb619f7e04a2287601827dd7dc68cf4
David Moates <david.m.moates@gmail.com>
2019-11-22 21:36:15 -0600

    done with MVP

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,19 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Person = styled.div`
+  background: rgba(255, 256, 255, 0.5);
+  width: 20%;
+  height: auto;
+  margin: 10px;
+`;
+
+const Name = styled.h2`
+  color: black;
+  text-height: 20px;
+`;
+
+const NoBullets = styled.ul`
+  list-style-type: none;
+
+`
+
+const PersonTrait = styled.li`
+  color: black;
+  text-height: 10px;
+  text-align: left;
+`;
+
+
 
 const StarWarsCard = (props) => {
 
   return (
-    <div>
-      <h2>{props.person.name}</h2>
-      <ul>
-        <li>{props.person.birth_year}</li>
-        <li>{props.person.height}</li>
-        <li>{props.person.mass}</li>
-        <li>{props.person.hair_color}</li>
-        <li>{props.person.eye_color}</li>
-      </ul>
-      
-    </div>
+    <Person>
+      <Name>{props.person.name}</Name>
+      <NoBullets>
+        <PersonTrait>Year Born:  {props.person.birth_year}</PersonTrait>
+        <PersonTrait>Height:  {props.person.height}</PersonTrait>
+        <PersonTrait>Weight:  {props.person.mass}</PersonTrait>
+        <PersonTrait>Hair Color:  {props.person.hair_color}</PersonTrait>
+        <PersonTrait>Eye Color:  {props.person.eye_color}</PersonTrait>
+      </NoBullets>
+
+    </Person>
   )
 
 }
